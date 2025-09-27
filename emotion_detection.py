b34f13238dbacbe7933a750da72b52ee8d717ef5
jblakeAI <jolie.blake@gmail.com>
2025-09-27 06:53:52 -0400

    Formatting Application Output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests 
+import json
 
 
 def emotion_detector(text_to_analyze):
@@ -6,4 +7,17 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} 
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=myobj, headers=headers)
-    return response.text
+    
+    # Response output is formatted to python dictionary 
+    formatted_response = json.loads(response.text)
+    
+    # Extracting emotions from formatted_response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+   
+    # Finding dominant emotion
+    dominant_emotion = max(emotions, key = emotions.get)
+    
+    # Updating emotions dictionary to include dominant resposne
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions

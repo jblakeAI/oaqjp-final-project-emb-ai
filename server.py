b97e2c75850ccb5ebe6a8d08ab80a96493d3f330
jblakeAI <jolie.blake@gmail.com>
2025-09-27 16:18:11 -0400

    Static Code Analysis

--- server.py
+++ server.py
@@ -33,27 +33,20 @@ def emo_detector():
         joy_score = response['joy']
         sadness_score = response['sadness']
         dom_emo = response['dominant_emotion']
-    
         return (f'For the given statement, the system response is '
             f'anger: {anger_score}, disgust: {disgust_score}, '
             f'fear: {fear_score}, joy: {joy_score} and '
             f'sadness: {sadness_score}. The dominant emotion is <b> {dom_emo}</b>.')
 
-    else:
-        return "Invalid text! Please try again!"
-    
+    return "Invalid text! Please try again!"
 
 
 @app.route("/")
 def render_index_page():
-        ''' This function initiates the rendering of the main application
-        page over the Flask channel
+    ''' This function initiates the rendering of the main application
+    page over the Flask channel
     '''
-        return render_template('index.html')
+    return render_template('index.html')
 
-
-
-''' This functions executes the flask app and deploys it on localhost:5000
-    '''
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=5000)
